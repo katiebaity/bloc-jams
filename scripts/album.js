90059b2d2a41295649a89984fd1f604f6e874363
Katherine Hayes <kmay.hayes@gmail.com>
2018-01-16 18:00:23 -0500

    bug fix

--- scripts/album.js
+++ scripts/album.js
@@ -130,7 +130,6 @@ var songRows = document.getElementsByClassName('album-view-song-item');
 
       songListContainer.addEventListener('mouseover', function(event) {
         if (event.target.parentElement.className === 'album-view-song-item') {
-          event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
 +            var songItem = getSongItem(event.target);
 +
 +            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
