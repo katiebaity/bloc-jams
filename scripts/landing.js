0dc3b1a8a13d39074956c01e054627b509f0490d
Katherine Hayes <kmay.hayes@gmail.com>
2018-01-18 14:34:49 -0500

    Landing Page Assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,23 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(point) {
-    points.style.opacity = 1;
-    points.style.transform = "scaleX(1) translateY(0)";
-    points.style.msTransform = "scaleX(1) translateY(0)";
-    points.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+  var revealPoint = function() {
+      $(this).css({
+          opacity: 1,
+          transform: 'scaleX(1) translateY(0)'
+    });
 };
 
-var animatePoints = function(points) {
-  forEach(points, revealPoint);
-};
+$.each($('.point'), revealPoint);
 
-window.onload = function() {
-  if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+  if ($(window).height() > 950) {
+           animatePoints();
      }
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+     $(window).scroll(function(event) {
+       if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
       }
   });
- }
+
+});
